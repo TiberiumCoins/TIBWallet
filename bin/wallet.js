3cafc75176e91c030fd8071f75ccc3a83eeaebf4
AlexMog <alexmog@live.fr>
2018-02-01 00:15:43 +0100

    Fixed async function

--- bin/wallet.js
+++ bin/wallet.js
@@ -27,75 +27,76 @@ all
 	    Show all current balances on the network
 	    `)
 
-let privkey
+async function main() {
+  let privkey
 
-try {
-  // load existing key
-  let privkeyContents = fs.readFileSync(keyPath, 'utf8')
-  let privkeyHex = JSON.parse(privkeyContents)[0].private
-  privkey = Buffer.from(privkeyHex, 'hex')
+  try {
+    // load existing key
+    let privkeyContents = fs.readFileSync(keyPath, 'utf8')
+    let privkeyHex = JSON.parse(privkeyContents)[0].private
+    privkey = Buffer.from(privkeyHex, 'hex')
 
-} catch (err) {
-  if (err.code !== 'ENOENT') throw err
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err
 
-  // no key, generate one
-  let keys = [ { private: randomBytes(32).toString('hex') } ]
-  let keysJson = JSON.stringify(keys, null, '  ')
-  mkdirp(dirname(keyPath))
-  fs.writeFileSync(keyPath, keysJson, 'utf8')
-  privkey = Buffer.from(keys[0].private, 'hex')
+    // no key, generate one
+    let keys = [ { private: randomBytes(32).toString('hex') } ]
+    let keysJson = JSON.stringify(keys, null, '  ')
+    mkdirp(dirname(keyPath))
+    fs.writeFileSync(keyPath, keysJson, 'utf8')
+    privkey = Buffer.from(keys[0].private, 'hex')
 
-  console.log(`Generated private key, saving to "${keyPath}"`)
-}
-
-let timeout = setTimeout(() => console.log('Connecting...'), 2000)
-
-let nodes = config.peers.map((addr) => `ws://${addr}:46657`)
-
-let client = await connect(null, { genesis, nodes })
-let wallet = Wallet(privkey, client)
-
-// don't print "Connecting..." if we connect in less than 2s
-clearTimeout(timeout)
-
-var stdin = process.openStdin();
-
-stdin.addListener("data", async function(d) {
-  var val = d.toString().trim();
-  var cmd = val.split(" ");
-    // send
-  if (cmd.length === 3 && cmd[0] === 'send') {
-    let recipientAddress = cmd[1]
-    let amountToSend = Number(cmd[2]) * 1e8
-
-    let res = await wallet
-      .send(recipientAddress, amountToSend)
-    console.log('done', res)
+    console.log(`Generated private key, saving to "${keyPath}"`)
   }
 
-  // get balance
-  if (cmd.length === 1 && cmd[0] === 'balance') {
-    let balance
-    try {
-      balance = await wallet.getBalance()
-    } catch (err) {
-      if (err.message === 'invalid state from full node') {
-        // retry if we get this error
+  let timeout = setTimeout(() => console.log('Connecting...'), 2000)
+
+  let nodes = config.peers.map((addr) => `ws://${addr}:46657`)
+
+  let client = await connect(null, { genesis, nodes })
+  let wallet = Wallet(privkey, client)
+
+  // don't print "Connecting..." if we connect in less than 2s
+  clearTimeout(timeout)
+
+  var stdin = process.openStdin();
+
+  stdin.addListener("data", async function(d) {
+    var val = d.toString().trim();
+    var cmd = val.split(" ");
+      // send
+    if (cmd.length === 3 && cmd[0] === 'send') {
+      let recipientAddress = cmd[1]
+      let amountToSend = Number(cmd[2]) * 1e8
+
+      let res = await wallet
+        .send(recipientAddress, amountToSend)
+      console.log('done', res)
+    }
+
+    // get balance
+    if (cmd.length === 1 && cmd[0] === 'balance') {
+      let balance
+      try {
         balance = await wallet.getBalance()
-      } else {
-        throw err
+      } catch (err) {
+        if (err.message === 'invalid state from full node') {
+          // retry if we get this error
+          balance = await wallet.getBalance()
+        } else {
+          throw err
+        }
       }
+      console.log('Address: ' + wallet.address)
+      console.log(`Balance: ${balance / 1e8} TIB`)
     }
-    console.log('Address: ' + wallet.address)
-    console.log(`Balance: ${balance / 1e8} TIB`)
-  }
 
-    if (cmd[0] == "all") {
-  var accounts = (await client.getState()).accounts;
-  Object.keys(accounts).forEach(function(address) {
-      console.log(address + ": " + (accounts[address].balance / 1e8) + " TIB")
+      if (cmd[0] == "all") {
+    var accounts = (await client.getState()).accounts;
+    Object.keys(accounts).forEach(function(address) {
+        console.log(address + ": " + (accounts[address].balance / 1e8) + " TIB")
+    });
+      }
   });
-    }
-});
-
+}
 main().catch((err) => console.error(err.stack))
